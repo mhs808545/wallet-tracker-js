3649dbd7f4d5952b8b7beaa0ceef637421b9817b
mhs <mhs808545@gmail.com>
2020-06-14 13:53:21 -0700

    fetch user info in display page and display it

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,31 @@
-console.log("hwllo");
+var auth = firebase.auth();
+var firestore = firebase.firestore();
+var nameDiv = document.querySelector(".name h3")
+
+
+//fetching uid from url 
+var uid = location.hash.substring(1,location.hash.length);
+// console.log(data.createdAt.toDate().toISOString().split("T")[0]); // date ko samghane k lye jo ajeeb si form me arhi
+
+var fetchUserInfo = async (uid) => {
+    try {
+        var userInfo = await firestore.collection("users").doc(uid).get();
+        return userInfo.data();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// fetchUserInfo(uid)
+
+//auth listner
+auth.onAuthStateChanged( async (user) => {
+    if (user) {
+        var {uid} = user;
+        var userInfo = await fetchUserInfo(uid);
+        // setting user info
+        nameDiv.textContent = userInfo.fullName;
+    } else {
+        console.log("user logged out");
+    }
+})
